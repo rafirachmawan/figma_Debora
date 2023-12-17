4e6d1dde607f2f849e88a0587c214e6020bf7160
rafirachmawan <rafirachmawan1987@gmail.com>
2023-12-18 04:57:53 +0700

    add navbar in background

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "./style.css";
 import background from "../../assets/jolt-landing-page-bg.png";
+// import Nav from "react-bootstrap/Nav";
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={background} alt="" />
-      <h1>Judul website</h1>
-      <h2>Subjudul atau Deskripsi</h2>
-      <nav>
-        <a href="#">Beranda</a>
-        <a href="#">Tentang</a>
-        <a href="#">Kontak</a>
-      </nav>
+    <div className="containerStyle">
+      <img className="imageStyle" src={background} alt="" />
+      <div className="contentStyle">
+        <a href="">Jolt</a>
+        <a href="">Categories</a>
+        <a href="">Collections</a>
+        <a href="">Store</a>
+        <a href="">Blog</a>
+        <a href="">Find Store</a>
+
+        <a href="">Search</a>
+        <a href="">Car</a>
+        <a href="">Login</a>
+      </div>
     </div>
   );
 };
